818fb95ab4b4be86463faa2e91005d69b7813f73
Nikita Hodyna <creedbroth@gmail.com>
2026-06-20 17:47:37 +0200

    Implement super and sub classes

--- app/main.py
+++ app/main.py
@@ -1,1 +1,46 @@
-# write your code here
+from __future__ import annotations
+
+
+class Animal:
+    alive = []
+
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False
+    ) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+
+    def bite(self, other: Herbivore) -> None:
+        if not isinstance(other, Carnivore):
+            if not other.hidden:
+                other.health -= 50
+            if other.health <= 0:
+                Animal.alive.remove(other)
+
+
+# rabbit = Herbivore("Rabbit")
+# print(rabbit.hidden)
+# lion = Carnivore("Lion")
+#
+# print(lion.bite(rabbit))
+print(Animal.alive)
